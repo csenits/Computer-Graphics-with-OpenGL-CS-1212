06663d0309a7d80a110a51c59e5d865dc94aec17
Abdullah Khilji <abdullahkhilji.nits@gmail.com>
2019-02-08 11:41:54 +0530

    Add files via upload

---
+++ 2018_01_23/lab_2.c
@@ -0,0 +1,59 @@
+// Lab 1: 23 January 2019
+// Write a OpenGL Program to create a display window and draw a rectangle within it.
+
+
+// Initialization Function
+#include<GL/glut.h>
+void myInit ()
+{  
+	// Resest background color with black (since all three argument is 0.0)	
+	glClearColor(0.0,0.0,0.0,1.0) ;
+	glMatrixMode(GL_PROJECTION); //Set window size in X and Y direction
+	gluOrtho2D(-780, 780, -420, 420);
+}
+	
+
+
+// Function to display animation	
+
+void lineSegment(void)
+{    
+	glClear(GL_COLOR_BUFFER_BIT); //clear display window
+	glColor3f(1.0,0.0,0.0); //set line segment color to red
+	glBegin(GL_LINES); 
+	
+	glVertex2i(0,0);   //specify line-segment geometry
+	glVertex2i(100,0);
+	
+	glVertex2i(0,100);
+	glVertex2i(100,100);
+	
+	glVertex2i(0,0);
+	glVertex2i(0,100);
+
+	glVertex2i(100,100);
+	glVertex2i(100,0);
+
+	
+	glEnd();
+	glFlush();   
+}
+
+
+int main(int argc, char** argv) 
+ {
+	glutInit(&argc, argv); // Display which is of RGB (Red Green Blue) type
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);  //Declares window size
+	glutInitWindowSize(1368,768); //set display window width and height
+	glutInitWindowPosition(0,0); // Declares window position which is (0,0)
+	glutCreateWindow("A Rectangle"); // Name to Window
+	myInit( );  // Call to myInit()
+	glutDisplayFunc(lineSegment); //send graphics to display window
+	glutMainLoop( ); //Display everything and wait
+
+}
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Computer-Graphics-with-OpenGL-CS-1212
 CS-1212 Lab Class of CS-1206 Computer Graphics
+
+
+Compile:
+
+## For Linux:
+
+
+```
+gcc -o filename filename.c -lGL -lGLU -lglut
+
+```
+
+## On macOS:
+### You need to change the header files to:
+
+```
+#include <OpenGL/gl.h> 
+#include <OpenGL/glu.h> 
+#include <GLUT/glut.h> 
+```
+
+and compile using:
+
+```
+gcc -o filename filename.c -framework Carbon -framework OpenGL -framework GLUT
+
+```
+
+## On Windows:
+
+### Type the following in cygwin shell with GLUT installed
+
+```
+gcc -o filename.exe filename.c -lopengl32 -lglut32
+```
+
+
+
